f8db8dd48723570a98e9f4c918043a170edef443
Craig Morris <craig.michael.morris@gmail.com>
2011-12-24 19:37:20 +1100

    Initial release...

---
+++ Lib/AirbrakeError.php
@@ -0,0 +1,35 @@
+<?php
+
+App::import('Vendor', 'AirbrakeCake.Airbrake_Configuration', array('file' => 'php-airbrake' . DS . 'src' . DS . 'Airbrake'. DS . 'Configuration.php'));
+App::import('Vendor', 'AirbrakeCake.Airbrake_Client', array('file' => 'php-airbrake' . DS . 'src' . DS . 'Airbrake'. DS . 'Client.php'));
+
+class AirbrakeError extends ErrorHandler
+{
+    public static function handleError($code, $description, $file = null, $line = null, $context = null) 
+    {
+    	// Call Airbrake
+		$apiKey  = Configure::read('AirbrakeCake.apiKey'); // This is required
+		$options = array(); // This is optional
+
+		$config = new Airbrake\Configuration($apiKey, $options);
+		$client = new Airbrake\Client($config);
+		$client->notifyOnError($description);
+        
+        // Fall back to cake
+        return parent::handleError($code, $description, $file, $line, $context);
+    }
+    
+    public static function handleException($exception) 
+    {
+    	// Call Airbrake
+		$apiKey  = Configure::read('AirbrakeCake.apiKey'); // This is required
+		$options = array(); // This is optional
+
+		$config = new Airbrake\Configuration($apiKey, $options);
+		$client = new Airbrake\Client($config);
+		$client->notifyOnException($exception);
+
+    	// Fall back to Cake..
+		return parent::handleException($exception);
+    }
+}
